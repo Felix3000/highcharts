9d272df1d265ff26644c9950d5cd269621577ede
Felix3000 <Felix2000@felixgrsmacbook.fritz.box>
2014-05-16 18:43:14 +0200

    initial
    initial

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<script src="js/jquery-1.8.3.min.js"></script>
+
+
+
+<title>
+Green Cockpit 2.0
+
+</title>
+
+</head>
+
+<body>
+
+<div id="container" class="" style="width:100%; height:400px";></div>
+
+</body>
+
+
+
+<script src="drawchart.js"></script>
+<script src="js/highcharts.js"></script>
+
+</html>
+
+
+
+
